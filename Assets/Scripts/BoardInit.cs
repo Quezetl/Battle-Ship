0aad832379bcceae873ac62e79c812e6d83d0202
Quezetl <bradleyberning@yahoo.com>
2022-12-15 13:46:31 -0800

    [Addition]: added coroutines to handle turn flow

--- Assets/Scripts/BoardInit.cs
+++ Assets/Scripts/BoardInit.cs
@@ -1,14 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using static UnityEditor.Experimental.GraphView.GraphView;
 
-public class PlayerBoardSetup : MonoBehaviour
+
+public class BoardInit : MonoBehaviour
 {
-    public bool[,] playerBoard = new bool[10, 10];
-    public GameObject GameManager;
+    public GameObject Water;
     public GameObject boardPrefab;
-    public GameObject[] playerShips;
+    public GameObject[] Ships;
     public GameObject shipPreviewPrefab;
     public GameObject mouseHighlighter;
     public LayerMask layer;
@@ -22,6 +23,10 @@ public class PlayerBoardSetup : MonoBehaviour
     GameObject[] cubePreview = new GameObject[5];
     ships[] plyrShip = new ships[5];
 
+    int i = 0;
+
+    public bool[,] boardObj = new bool[20, 11];
+    ships[] botShip = new ships[5];
 
 
     public struct ships
@@ -45,22 +50,39 @@ public class PlayerBoardSetup : MonoBehaviour
         public GameObject getShipObj() { return shipObj; }
         public void init(GameObject obj, int length, string name) { shipObj = obj; shipLength = length; shipName = name; placed = false; orientation = 0; validPlacement = true; }
     }
+    
+    void PlaceWater()
+    {
+        GameObject.Instantiate(Water).transform.position = new Vector3(10, 0.100000001f, 5.5f);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        shipSync();
+        PlaceWater();
+        aiShipSync();
         boardInit();
+        aiShipCheck();
+        shipSync();
     }
-
     // Update is called once per frame
     void Update()
     {
-
         bool onScreen = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out mousePos);
         for (int i = 0; i < 5; i++)
         {
             cubePreview[i].SetActive(false);
         }
+        if (!onScreen)
+        {
+            mouseHighlighter.SetActive(false);
+            return;
+        }
+        if (!mouseOnBoard(mousePos))
+        {
+            mouseHighlighter.SetActive(false);
+        }
+
         if (onScreen && !allPiecesPlaced())
         {
             if (!mouseOnBoard(mousePos))
@@ -79,6 +101,12 @@ public class PlayerBoardSetup : MonoBehaviour
             }
 
         }
+        if (allPiecesPlaced() && i ==0)
+        {
+            this.GetComponent<GameLoop>().enabled = true;
+            this.GetComponent<GameLoop>().setBoard(boardObj);
+            i++;
+        }
 
     }
     
@@ -88,7 +116,7 @@ public class PlayerBoardSetup : MonoBehaviour
         int j = 2;
         for (int i = 0; i < 5; i++)
         {
-            plyrShip[i].init(playerShips[i], j, playerShips[i].name);
+            plyrShip[i].init(Ships[i], j, Ships[i].name);
 
             if (i != 1)
                 j++;
@@ -111,25 +139,41 @@ public class PlayerBoardSetup : MonoBehaviour
 
     void boardInit()
     {
-        for (int i = 0; i < 10; i++)
+        GameObject tmp;
+
+        for (int i = 0; i < 20; i++)
         {
-            for (int j = 0; j < 10; j++)
+            for (int j = 0; j < 11; j++)
             {
-                playerBoard[i, j] = true;
+                boardObj[i, j] = true;
             }
         }
 
-        GameObject tmp;
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < 21; i++)
         {
-            for (int j = 1; j < 11; j++)
+            if (i < 10)
             {
-                string objname = $"({i},{j})";
-                string textname = $"({i + 1},{j + 1})";
-                tmp = GameObject.Instantiate(this.boardPrefab, new Vector3(i, 0, j), this.boardPrefab.transform.rotation) as GameObject;
-                tmp.GetComponentInChildren<TMPro.TextMeshProUGUI>(tmp).text = textname;
-                tmp.name = objname;
-                tmp.tag = "Base";
+                for (int j = 1; j < 11; j++)
+                {
+                    string objname = $"({i},{j})";
+                    string textname = $"({i + 1},{j })";
+                    tmp = GameObject.Instantiate(this.boardPrefab, new Vector3(i, 0, j), this.boardPrefab.transform.rotation) as GameObject;
+                    tmp.GetComponentInChildren<TMPro.TextMeshProUGUI>(tmp).text = textname;
+                    tmp.name = objname;
+                    tmp.tag = "Base";
+                }
+            }
+            else if( i > 10)
+            {
+                for (int j = 1; j < 11; j++)
+                {
+                    string objname = $"({i},{j})";
+                    string textname = $"({i},{j})";
+                    tmp = GameObject.Instantiate(this.boardPrefab, new Vector3(i, 0, j), this.boardPrefab.transform.rotation) as GameObject;
+                    tmp.GetComponentInChildren<TMPro.TextMeshProUGUI>(tmp).text = textname;
+                    tmp.name = objname;
+                    tmp.tag = "aiBase";
+                }
             }
         }
     }
@@ -214,6 +258,7 @@ public class PlayerBoardSetup : MonoBehaviour
         float acom = 0;
         float x = mousePos.transform.position.x;
         float z = mousePos.transform.position.z;
+        float y = 0;
         GameObject shipPiece = GameObject.Instantiate(plyrShip[curShip].getShipObj());
         float zBounds = shipPiece.GetComponent<Collider>().bounds.size.z;
         float xBounds = shipPiece.GetComponent<Collider>().bounds.size.x;
@@ -227,34 +272,54 @@ public class PlayerBoardSetup : MonoBehaviour
         else
             acom = 0;
 
+        switch (curShip)
+        {
+            case 0:
+                y = 0.672f;
+                break;
+            case 1:
+                y = 0.821f;
+                break;
+            case 2:
+                y = 0.842f;
+                break;
+            case 3:
+                y = 0.5f;
+                break;
+            case 4:
+                y = 0.622f;
+                break;
+        }
 
         switch (orientation)
         {
             case 0:
-                shipPiece.transform.position = mousePos.transform.position + new Vector3(0f, 0f, zBounds / (3 - acom));
+                shipPiece.transform.position = mousePos.transform.position + new Vector3(0f, 0, zBounds / (3 - acom));
                 shipPiece.transform.Rotate(0, -180, 0);
                 break;
             case 1:
-                shipPiece.transform.position = mousePos.transform.position + new Vector3(zBounds / (3 - acom), 0f, 0f);
+                shipPiece.transform.position = mousePos.transform.position + new Vector3(zBounds / (3 - acom), 0, 0f);
                 shipPiece.transform.Rotate(0, -90, 0);
                 break;
             case 2:
-                shipPiece.transform.position = mousePos.transform.position - new Vector3(0f, 0f, zBounds / (3 - acom));
+                shipPiece.transform.position = mousePos.transform.position - new Vector3(0f, 0, zBounds / (3 - acom));
                 shipPiece.transform.Rotate(0, 0, 0);
                 break;
             case 3:
-                shipPiece.transform.position = mousePos.transform.position - new Vector3(zBounds / (3 - acom), 0f, 0f);
+                shipPiece.transform.position = mousePos.transform.position - new Vector3(zBounds / (3 - acom), 0, 0f);
                 shipPiece.transform.Rotate(0, -270, 0);
                 break;
         };
 
-        for (int i = 0; i < plyrShip[curShip].getLength(); i++)
+       /* for (int i = 0; i < plyrShip[curShip].getLength(); i++)
         {
             GameObject shipCarpet = GameObject.Instantiate(mouseHighlighter);
+            shipCarpet.name = $"green carpet {i} for {plyrShip[curShip].getName()}";
             shipCarpet.GetComponent<Renderer>().material = greenCarpet;
-            shipCarpet.transform.position = mousePos.transform.position + orientVec[i];
+            shipCarpet.transform.position = mousePos.transform.position + orientVec[i] + new Vector3(0, 0.6f, 0);
+            shipCarpet.layer = 3;
 
-        }
+        }*/
 
         plyrShip[curShip].updatePlaced(true);
         for (int i = 0; i < 5; i++)
@@ -268,33 +333,34 @@ public class PlayerBoardSetup : MonoBehaviour
 
     void updateBoard(int x, int y, int orient)
     {
+        y--;
         switch (orient)
         {
             case 0:
                 for (int i = 0; i < plyrShip[curShip].getLength(); i++)
                 {
-                    playerBoard[x, y] = false;
+                    boardObj[x, y] = false;
                     y++;
                 }
                 break;
             case 1:
                 for (int i = 0; i < plyrShip[curShip].getLength(); i++)
                 {
-                    playerBoard[x, y] = false;
+                    boardObj[x, y] = false;
                     x++;
                 }
                 break;
             case 2:
                 for (int i = 0; i < plyrShip[curShip].getLength(); i++)
                 {
-                    playerBoard[x, y] = false;
+                    boardObj[x, y] = false;
                     y--;
                 }
                 break;
             case 3:
                 for (int i = 0; i < plyrShip[curShip].getLength(); i++)
                 {
-                    playerBoard[x, y] = false;
+                    boardObj[x, y] = false;
                     x--;
                 }
                 break;
@@ -346,4 +412,172 @@ public class PlayerBoardSetup : MonoBehaviour
     {
         curShip = val;
     }
+
+    void aiShipSync()
+    {
+        int j = 2;
+        for (int i = 0; i < 5; i++)
+        {
+            botShip[i].init(GameObject.Instantiate(Ships[i]), j, Ships[i].name);
+
+            if (i != 1)
+                j++;
+
+        }
+    }
+
+
+    void boardFill(int x, int y, int orientation, int length)
+    {
+        for (int i = 0; i < length; i++)
+        {
+            switch (orientation)
+            {
+                case 0:
+                    boardObj[x, y] = false;
+                    y++;
+                    break;
+                case 1:
+                    boardObj[x, y] = false;
+                    x++;
+                    break;
+                case 2:
+                    boardObj[x, y] = false;
+                    y--;
+                    break;
+                case 3:
+                    boardObj[x, y] = false;
+                    x--;
+                    break;
+            }
+        }
+    }
+    void aiShipCheck()
+    {
+        int orientation;
+        int x;
+        int y;
+
+        for (int i = 0; i < 5; i++)
+        {
+            do
+            {
+                orientation = (int)Random.Range(0.0f, 4.0f);
+                x = (int)Random.Range(11.0f, 20.0f);
+                y = (int)Random.Range(1.0f, 10.0f);
+            } while (!validPosition(x, y, orientation, i));
+            botShip[i].setCoord(new Vector3(x, 0, y));
+            boardFill(x, y, orientation, botShip[i].getLength());
+            aiShipPlace(i, orientation);
+
+        }
+
+
+    }
+
+    void aiShipPlace(int curShip, int orientation)
+    {
+        float acom = 0;
+        float x = botShip[curShip].getCoord().x;
+        float z = botShip[curShip].getCoord().z;
+        float y = 0;
+        GameObject shipPlacement = botShip[curShip].getShipObj();
+        float zBounds = shipPlacement.GetComponent<Collider>().bounds.size.z;
+        float xBounds = shipPlacement.GetComponent<Collider>().bounds.size.x;
+        float yBounds = shipPlacement.GetComponent<Collider>().bounds.size.y;
+        Vector3 shipBounds = new Vector3(xBounds, yBounds, zBounds);
+
+        shipPlacement.name = "(" + botShip[curShip].getCoord().x.ToString() + ", " + botShip[curShip].getCoord().z.ToString() + ")" + " Length:" + botShip[curShip].getLength() + "orientation: " + orientation;
+
+
+
+        if (curShip == 4)
+            acom = 1;
+        else if (curShip == 3)
+            acom = .6f;
+        else
+            acom = 0;
+        switch (orientation)
+        {
+            case 0:
+                shipPlacement.transform.position = new Vector3(x, y, z) + new Vector3(0f, 0f, zBounds / (3 - acom));
+                shipPlacement.transform.Rotate(0, -180, 0);
+                break;
+            case 1:
+                shipPlacement.transform.position = new Vector3(x, y, z) + new Vector3(zBounds / (3 - acom), 0f, 0f);
+                shipPlacement.transform.Rotate(0, -90, 0);
+                break;
+            case 2:
+                shipPlacement.transform.position = new Vector3(x, y, z) - new Vector3(0f, 0f, zBounds / (3 - acom));
+                shipPlacement.transform.Rotate(0, 0, 0);
+                break;
+            case 3:
+                shipPlacement.transform.position = new Vector3(x, y, z) - new Vector3(zBounds / (3 - acom), 0f, 0f);
+                shipPlacement.transform.Rotate(0, -270, 0);
+                break;
+        };
+        // UNCOMMENT LATER
+        //shipPlacement.setActive(False);
+        shipPlacement.GetComponent<MeshCollider>().enabled = false;
+    }
+
+    bool validPosition(int x, int y, int orient, int curBoat)
+    {
+        float bounds = botShip[curBoat].getLength();
+        switch (orient)
+        {
+            case 0:
+                if (y + botShip[curBoat].getLength() > 10)
+                    return false;
+                break;
+            case 1:
+                if (x + botShip[curBoat].getLength() > 20)
+                    return false;
+                break;
+            case 2:
+                if (y - botShip[curBoat].getLength() < 0)
+                    return false;
+                break;
+            case 3:
+                if (x - botShip[curBoat].getLength() < 11)
+                    return false;
+                break;
+        }
+
+        if (curBoat == 0)
+            return true;
+
+        for (int i = 0; i < botShip[curBoat].getLength(); i++)
+        {
+            switch (orient)
+            {
+                case 0:
+                    if (!boardObj[x, y])
+                        return false;
+                    y++;
+                    break;
+                case 1:
+                    if (!boardObj[x, y])
+                        return false;
+                    x++;
+                    break;
+                case 2:
+                    if (!boardObj[x, y])
+                        return false;
+                    y--;
+                    break;
+                case 3:
+                    if (!boardObj[x, y])
+                        return false;
+                    x--;
+                    break;
+            }
+        }
+        return true;
+    }
+
+
+
+
+
 }
--- Assets/Scripts/GameLoop.cs
+++ Assets/Scripts/GameLoop.cs
@@ -1,47 +1,167 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEditorInternal;
 using UnityEngine;
+
+public enum GameState { START, TURNDECISION, PLAYERTURN, AITURN, WON, LOST }
+
 
 public class GameLoop : MonoBehaviour
 {
-    public GameObject shipObjcts;
+    public GameObject Notification;
+    public RaycastHit mousePos;
+    public GameObject highlighter;
+    public GameState state;
+    public GameObject firstTurnPrompt;
+    public bool[,] Board = new bool[20, 11];
+    public struct ships
+    {
+        GameObject shipObj;
+        int shipLength;
+        string shipName;
+        Vector3 coord;
+        bool placed;
+        int orientation;
+        bool validPlacement;
+        public bool isValid() { return validPlacement; }
+        public void updateValid(bool validVal) { validPlacement = validVal; }
+        public bool isPlaced() { return placed; }
+        public int getLength() { return shipLength; }
+        public string getName() { return shipName; }
+        public Vector3 getCoord() { return coord; }
+        public int getOrientation() { return orientation; }
+        public void updatePlaced(bool placed) { this.placed = placed; }
+        public void setCoord(Vector3 coord) { this.coord = coord; }
+        public GameObject getShipObj() { return shipObj; }
+        public void init(GameObject obj, int length, string name) { shipObj = obj; shipLength = length; shipName = name; placed = false; orientation = 0; validPlacement = true; }
+    }
 
-
-    public bool[,] plyrBoard = new bool[10, 10];
-    public bool[,] aiBoard = new bool[20, 10];
-
-    bool gameStart = false;
-    bool playerShipsSet = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        highlighter = GameObject.Instantiate(highlighter);
+        highlighter.SetActive(false);
+        state = GameState.START;
+        Invoke("WhoGoesFirst", 2);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!checkGameReady())
+        bool onScreen = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out mousePos);
+
+        if (!onScreen)
         {
+            highlighter.SetActive(false);
             return;
         }
-        if (!playerShipsSet)
+        if (!mousePos.collider.CompareTag("aiBase"))
         {
-            setShipLayout();
-            playerShipsSet = true;
+            highlighter.SetActive(false);
+        }
+        
+
+        if (mousePos.collider.CompareTag("aiBase")) 
+        {
+            highlighter.SetActive(true);
+            highlighter.transform.position = mousePos.transform.position; 
+        }
+    }
+
+
+
+    void WhoGoesFirst()
+    {
+        firstTurnPrompt.SetActive(true);
+    }
+
+    public void onPlayerButton()
+    {
+        firstTurnPrompt.SetActive(false);
+        state = GameState.PLAYERTURN;
+
+        StartCoroutine(DisplayNotification("You Shall go first"));
+    }
+
+    public void onAIButton()
+    {
+        firstTurnPrompt.SetActive(false);
+        state = GameState.AITURN;
+
+        StartCoroutine(DisplayNotification("AI Shall go first"));
+
+    }
+
+    IEnumerator DisplayNotification(string message)
+    {
+        Notification.SetActive(true);
+        Notification.GetComponentInChildren<TMPro.TextMeshProUGUI>(Notification).text = message;
+        yield return new WaitForSeconds(3);
+        Notification.SetActive(false);
+        if (state == GameState.PLAYERTURN)
+        {
+            StartCoroutine(PlayerTurn());
+        }
+        else if (state == GameState.AITURN)
+        {
+            StartCoroutine(AITurn());
+        }
+    }
+
+
+    IEnumerator AITurn()
+    {
+        int x = (int)Random.Range(1.0f, 10.0f);
+        int y = (int)Random.Range(1.0f, 10.0f);
+        // Animation for hit or miss goes here
+        Debug.Log("Do animation for hit or miss");
+
+        yield return new WaitForSeconds(3);
+
+        if (HitCheck(x, y))
+        {
+            StartCoroutine(DisplayNotification($"Ai has hit!"));
+        }
+        else
+        {
+            state = GameState.PLAYERTURN;
+            StartCoroutine(DisplayNotification($"Ai has missed!\nIt is now Your turn!"));            
+            // Do player turn
         }
 
-        Debug.Log("Game Start");
+    }
+    IEnumerator PlayerTurn()
+    {
+        yield return StartCoroutine(WaitForMouseDown());
+        int x = (int)highlighter.transform.position.x;
+        int y = (int)highlighter.transform.position.z;
+
+        //Do Animation for hit or miss here
+        Debug.Log("do hit or miss animation");
+        yield return new WaitForSeconds(3);
+
+        if (HitCheck(x, y))
+        {
+            StartCoroutine(DisplayNotification($"You hit!"));
+        }
+        else
+        {
+            state = GameState.AITURN;
+            StartCoroutine(DisplayNotification($"You have missed!\nIt is now the Ai's turn!"));
+
+            // Do player turn
+        }
     }
 
 
-    void setShipLayout()
-    {
-        plyrBoard = shipObjcts.GetComponent<PlayerBoardSetup>().playerBoard;
-        aiBoard = shipObjcts.GetComponent<AIShipPlace>().boardObj;
-    }
-    bool checkGameReady()
-    {
-        return shipObjcts.GetComponent<PlayerBoardSetup>().allPiecesPlaced();
-    }
+
+    bool HitCheck(int x, int y) { return !Board[x, y] ? true : false; }
+    public void setBoard(bool[,] board) { Board = board; }
+    IEnumerator WaitForMouseDown() { while (!Input.GetMouseButtonUp(0)) { yield return null; } }
+
 }
